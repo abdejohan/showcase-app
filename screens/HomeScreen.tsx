e14d94c984e40d140e599320d5df298fb962150b
abdejohan <abdejohan@gmail.com>
2023-01-24 12:41:21 +0100

    Display products in grid style

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,23 +1,61 @@
 import "react-native-gesture-handler";
-import { Text, View, Image, StyleSheet, Pressable } from "react-native";
+import { Text, View, Image, StyleSheet, Pressable, Dimensions } from "react-native";
 import globalStyles from "../styles";
 import { SharedElement } from "react-navigation-shared-element";
-import { items } from "../utils";
+import { products } from "../utils";
 import IntroScreen from "./IntroScreen";
+import Animated, { FadeOut } from "react-native-reanimated";
+import { useEffect, useState } from "react";
+import { Product } from "../types";
+import HeroProduct from "../components/HeroProduct";
+const { height: SCREEN_HEIGHT, width: SCREEN_WIDTH } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
-	heroContainer: {
+	titleContainer: {
+		backgroundColor: "#FCD867",
 		width: "100%",
-		height: "50%",
-		borderBottomLeftRadius: 30,
-		borderBottomRightRadius: 30,
-		zIndex: 2,
-		overflow: "hidden",
+		height: "20%",
+		top: -30,
+		padding: 30,
+		justifyContent: "center",
+		paddingTop: 50,
+		zIndex: 1,
 	},
-	heroImage: {
-		flex: 1,
+	hideContent: {
+		position: "absolute",
+		height: "100%",
 		width: "100%",
-		borderWidth: 1,
+		zIndex: 3,
+		backgroundColor: "black",
+	},
+	gridImageWrapper: {
+		top: -30,
+		flexGrow: 1,
+		width: "100%",
+		flexDirection: "row",
+		flexWrap: "wrap",
+	},
+	gridImage: {
+		height: "100%",
+		width: "100%",
+	},
+	bluryWrapper: {
+		position: "absolute",
+		width: "100%",
+		bottom: 0,
+		opacity: 0.8,
+		...globalStyles.darkestfade,
+		paddingHorizontal: 20,
+		paddingVertical: 10,
+	},
+	donutTitle: {
+		color: "white",
+		fontWeight: "bold",
+		fontSize: 16,
+	},
+	donutPrice: {
+		color: "white",
+		fontSize: 14,
 	},
 });
 
@@ -26,56 +64,53 @@ interface HomeProps {
 }
 
 const HomeScreen: React.FC<HomeProps> = ({ navigation }) => {
+	const [visible, setVisible] = useState(true);
+	const [layoutHeight, setLayoutHeight] = useState(0);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setVisible(false);
+		}, 4000);
+	}, []);
+
 	return (
 		<>
 			<IntroScreen />
 			<View style={globalStyles.container}>
-				<View
-					style={{
-						position: "absolute",
-						height: "100%",
-						width: "100%",
-						zIndex: 3,
-						backgroundColor: "black",
-					}}
-				/>
-				<Pressable
-					onPress={() => navigation.navigate("Overview", { item: items[0] })}
-					style={({ pressed }) => [{ ...styles.heroContainer }]}>
-					<SharedElement id={items[0].id} style={{ flex: 1 }}>
-						<Image
-							source={items[0].image}
-							style={styles.heroImage}
-							resizeMode='contain'
-						/>
-					</SharedElement>
-				</Pressable>
-
-				<View
-					style={{
-						backgroundColor: "#FCD867",
-						width: "100%",
-						height: "20%",
-						top: -30,
-						padding: 30,
-						justifyContent: "center",
-						paddingTop: 30,
-						borderBottomLeftRadius: 30,
-						borderBottomRightRadius: 30,
-					}}>
+				{visible && <Animated.View exiting={FadeOut} style={styles.hideContent} />}
+				<HeroProduct product={products[0]} />
+				<View style={styles.titleContainer}>
 					<Text style={globalStyles.title}>New Sweet Shop</Text>
-					<Text style={globalStyles.title}>Cupcake Box</Text>
+					<Text style={globalStyles.title}>Donut Box</Text>
 				</View>
 				<View
-					style={{
-						top: -60,
-						backgroundColor: "#36B3A3",
-						padding: 30,
-						borderRadius: 30,
+					style={{ ...styles.gridImageWrapper, height: layoutHeight }}
+					onLayout={(event) => {
+						layoutHeight === 0
+							? setLayoutHeight(event.nativeEvent.layout.height + 30)
+							: undefined;
 					}}>
-					<Text style={{ maxWidth: "90%", color: "white" }}>
-						Choose from our regular and mini cupcakes.
-					</Text>
+					{products.map(
+						(product: Product) =>
+							product.id !== "1" && (
+								<Pressable
+									key={product.id}
+									onPress={() => navigation.navigate("Overview", { product })}
+									style={({ pressed }) => [{ height: "50%", width: "50%" }]}>
+									<SharedElement id={product.id}>
+										<Image
+											source={product.image}
+											style={styles.gridImage}
+											resizeMode='stretch'
+										/>
+									</SharedElement>
+									<View style={styles.bluryWrapper}>
+										<Text style={styles.donutTitle}>{product.title}</Text>
+										<Text style={styles.donutPrice}>${product.price}</Text>
+									</View>
+								</Pressable>
+							)
+					)}
 				</View>
 			</View>
 		</>
--- utils/index.ts
+++ utils/index.ts
@@ -1,14 +1,50 @@
 import heroImage from "../assets/hero-image.jpg";
+import donut1 from "../assets/donut1.jpg";
+import donut2 from "../assets/donut2.jpg";
+import donut3 from "../assets/donut3.jpg";
+import donut4 from "../assets/donut4.jpg";
 
-const items = [
+const products = [
 	{
 		id: "1",
-		title: "Salted Donut",
-		description: "Vegan",
+		title: "Rasberry Donut",
+		description: "Salty and Sweet Donut with Rasberry Glaze",
 		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-		price: "2599",
+		price: "3,75",
 		image: heroImage,
+	},
+	{
+		id: "2",
+		title: "Chocolate Donut",
+		description: "Chocolate Glazed Donut",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		price: "3,25",
+		image: donut1,
+	},
+	{
+		id: "3",
+		title: "Bagel Donut",
+		description: "Chocolate Drizzled with Sprinkle filling",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		price: "3,25",
+		image: donut2,
+	},
+	{
+		id: "4",
+		title: "Swedish Summer",
+		description: "Strawberry Jam filled Donut with Double Cream",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		price: "3,25",
+		image: donut3,
+	},
+	{
+		id: "5",
+		title: "Mixed Pack",
+		description: "Mixed 12-pack of the Chefs Favorites",
+		text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		price: "35,99",
+		image: donut4,
 	},
 ];
 
-export { items };
+export { products };
